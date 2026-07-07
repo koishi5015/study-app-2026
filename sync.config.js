140f3ae76def99747feb66f5d877553898b760ac
koishi5015 <koutaroururu@gmail.com>
2026-07-07 21:03:12 +0900

    Add Firebase sync config

---
+++ sync.config.js
@@ -0,0 +1,11 @@
+window.STUDY_SYNC_CONFIG = {
+  firebaseSdkVersion: "10.12.5",
+  firebase: {
+    apiKey: "AIzaSyDL5yERl6hw8ke0YHhxaqG4YqHR0jfxWCc",
+    authDomain: "study-app-2026-5582a.firebaseapp.com",
+    projectId: "study-app-2026-5582a",
+    storageBucket: "study-app-2026-5582a.firebasestorage.app",
+    messagingSenderId: "527808006443",
+    appId: "1:527808006443:web:b98c3fed70e134ed685d6e"
+  }
+};
